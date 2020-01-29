b635f684be2a8d2768869782c2d450db9c460351
Michael Auchter <michael.auchter@ni.com>
2020-01-29 10:05:43 -0600

    zsh: move vcs info from right to left

--- zsh/.zsh/prompt.zsh
+++ zsh/.zsh/prompt.zsh
@@ -5,8 +5,8 @@ autoload -Uz vcs_info
 zstyle ':vcs_info:*' formats '%F{green}%s%F{7} %F{2}(%F{blue}%b%F{2})%f '
 zstyle ':vcs_info:*' enable git svn
 
-PS1="%F{green}%n@%m%k %F{blue}%1~ %# %b%f%k"
-RPROMPT='${vcs_info_msg_0_}'
+PS1="${vcs_info_msg_0_}%F{green}%n@%m%k %F{blue}%1~ %# %b%f%k"
+RPROMPT=''
 
 precmd() {
    title "zsh" "%m" "%55<...<%~"
