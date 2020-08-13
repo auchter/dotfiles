87d7a812e165c94667b524b3ce1a32a7e548c8c2
Michael Auchter <a@phire.org>
2020-08-13 01:41:15 +0000

    nixos: moloch: add initial config
    Need to factor out common laptop/desktop configs here, but it's a start.

---
+++ nixos/configuration.moloch.nix
@@ -0,0 +1,107 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      /etc/nixos/hardware-configuration.nix
+      ./base.nix
+      ./users.nix
+    ];
+
+  networking.hostName = "moloch";
+  networking.domain = "phire.org";
+  networking.wireless.enable = true;
+  networking.useDHCP = false;
+  networking.interfaces.enp0s31f6.useDHCP = true;
+  networking.interfaces.wlp2s0.useDHCP = true;
+
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+  boot.loader.grub.efiSupport = true;
+  boot.loader.grub.device = "nodev";
+
+  boot.initrd.luks.devices = {
+    root = {
+      device = "/dev/disk/by-uuid/0a0fa5cb-d138-44ca-bd12-bae076518b15";
+      preLVM = true;
+      allowDiscards = true;
+    };
+  };
+
+  hardware.pulseaudio.enable = true;
+
+  programs.sway = {
+    enable = true;
+    extraPackages = with pkgs; [
+      swaylock
+      swayidle
+      xwayland
+      waybar
+      mako
+      kanshi
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    alacritty
+    bc
+    brightnessctl
+    dmenu
+    i3blocks
+    mupdf
+    notify-desktop
+    powertop
+    qutebrowser
+    wob
+    (
+      pkgs.writeTextFile {
+        name = "startsway";
+        destination = "/bin/startsway";
+        executable = true;
+        text = ''
+          #! ${pkgs.bash}/bin/bash
+          systemctl --user import-environment
+          systemctl --user start sway.service
+        '';
+      }
+    )
+  ];
+
+  systemd.user.targets.sway-session = {
+    description = "Sway compositor";
+    documentation = [ "" ];
+    bindsTo = [ "graphical-session.target" ];
+    wants = [ "graphical-session-pre.target" ];
+    after = [ "graphical-session-pre.target" ];
+  };
+
+  systemd.user.services.sway = {
+    description = "Sway - Wayland WM";
+    documentation = [ "" ];
+    bindsTo = [ "graphical-session.target" ];
+    wants = [ "graphical-session-pre.target" ];
+    after = [ "graphical-session-pre.target" ];
+    environment.PATH = lib.mkForce null;
+    serviceConfig = {
+      Type = "simple";
+      ExecStart = ''
+        ${pkgs.dbus}/bin/dbus-run-session ${pkgs.sway}/bin/sway
+      '';
+      Restart = "on-failure";
+      RestartSec = 1;
+      TimeoutStopSec = 10;
+    };
+  };
+
+  programs.waybar.enable = true;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "20.03"; # Did you read the comment?
+}
