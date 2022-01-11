5aef5fb306cc663af778655adcedb63cec38cd41
Michael Auchter <a@phire.org>
2022-01-11 02:17:59 +0000

    nixos: orobas: setup storage and exports

--- nixos/configuration.orobas.nix
+++ nixos/configuration.orobas.nix
@@ -17,6 +17,66 @@
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/sdb";
 
+  fileSystems."/mnt/storage" = {
+    device = "/dev/disk/by-uuid/e56a3505-1ec1-4623-845f-b307b6a42e9d";
+    fsType = "ext4";
+  };
+
+  fileSystems."/export/music" = {
+    device = "/mnt/storage/music";
+    options = [ "bind" ];
+  };
+
+  fileSystems."/export/films" = {
+    device = "/mnt/storage/films";
+    options = [ "bind" ];
+  };
+
+  fileSystems."/export/tv" = {
+    device = "/mnt/storage/tv";
+    options = [ "bind" ];
+  };
+
+  services.nfs.server.enable = true;
+  services.nfs.server.exports = ''
+    /export/music 192.168.0.0/24(ro,no_subtree_check)
+    /export/tv 192.168.0.0/24(ro,no_subtree_check)
+    /export/films 192.168.0.0/24(ro,no_subtree_check)
+  '';
+
+  services.samba = {
+    enable = true;
+    securityType = "user";
+    extraConfig = ''
+      workgroup = PHIRE
+      server string = orobas
+      netbios name = orobas
+      security = user
+      hosts allow = 192.168.0.0/24 localhost
+      guest account = nobody
+    '';
+    shares = {
+      music = {
+        path = "/export/music";
+        browseable = "yes";
+        "read only" = "yes";
+        "guest ok" = "yes";
+      };
+      tv = {
+        path = "/export/tv";
+        browseable = "yes";
+        "read only" = "yes";
+        "guest ok" = "yes";
+      };
+      films = {
+        path = "/export/films";
+        browseable = "yes";
+        "read only" = "yes";
+        "guest ok" = "yes";
+      };
+    };
+  };
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
