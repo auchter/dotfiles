ebfc3c9bf8197e17544c0181169c05a90367f279
Michael Auchter <a@phire.org>
2022-01-11 02:20:09 +0000

    nixos: users: add additional ssh keys

--- nixos/users.nix
+++ nixos/users.nix
@@ -17,6 +17,8 @@
     uid = 1000;
     openssh.authorizedKeys.keys = [
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzIue421fOG3rtf/OrysL6lbGYuyavrZtQEPgXNkeYqQmBcHTGBujhXkr6fXVB+l4I2E+eWz+vyDD0JZWYZYeyyFSTS6yArIH+sEdEFzqNjQ+H0td5sEfysZfOkeGSKwNhnSKl5yVFkIn3DE3igCS97CqNBGf2kBLX//BGvgrvGLVvDIv6x0hUhNjHyTcth510sRi3TTcDb+GwNQqHyl9K/gtQFXVNVXg1bxGhzDD+NFARHiaPys6/QHP3N2X6KJV+1L3Of/w2+YLNPhRaJMBw3C4nrNoC/vPbH0m2pO3JhfILW7f7EgZSfp82vEO2XGuKbMc+1tXUaInA6Gy889f1 a@moloch.phire.org"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAICrmtSai6pNYXGbJX9BULfF7LpdfYWS/TDBAea27AxZDAAAABHNzaDo= a@moloch"
+      "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAII8U1HietB8llpiXaFNhZFxYlbrGjp53U4W/79qOufu4AAAABHNzaDo= a@moloch"
     ];
   };
 
