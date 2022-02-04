82df31a897be92373fdddfe30bbd18966f78bb9b
Michael Auchter <a@phire.org>
2022-02-04 01:21:04 +0000

    nixos: move bc to dev module

--- nixos/modules/dev.nix
+++ nixos/modules/dev.nix
@@ -2,6 +2,7 @@
 
 {
   environment.systemPackages = with pkgs; [
+    bc
     bmap-tools
     git
   ];
--- nixos/modules/sway.nix
+++ nixos/modules/sway.nix
@@ -21,7 +21,6 @@
 
   environment.systemPackages = with pkgs; [
     alacritty
-    bc
     brightnessctl
     dmenu
     exiftool
