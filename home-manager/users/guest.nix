830f2af1fdcc64b2b0c7b0d99440e66275a056ac
Michael Auchter <a@phire.org>
2022-02-16 18:36:39 +0000

    add guest

---
+++ home-manager/users/guest.nix
@@ -0,0 +1,23 @@
+{ config, pkgs, lib, ... }:
+
+{
+  imports = [
+    ./graphical.nix
+  ];
+
+  home.username = "guest";
+  home.homeDirectory = "/home/guest";
+
+  # This value determines the Home Manager release that your
+  # configuration is compatible with. This helps avoid breakage
+  # when a new Home Manager release introduces backwards
+  # incompatible changes.
+  #
+  # You can update Home Manager without changing this value. See
+  # the Home Manager release notes for a list of state version
+  # changes in each release.
+  home.stateVersion = "21.11";
+
+  # Let Home Manager install and manage itself.
+  programs.home-manager.enable = true;
+}
