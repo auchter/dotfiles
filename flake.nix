e27ed3d63f2e9c66e59ab3a7916e90e90c941e96
Michael Auchter <a@phire.org>
2022-02-16 16:07:47 +0000

    Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-21.11";
+    home-manager.url = "github:rycee/home-manager/release-21.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations = {
+      moloch = nixpkgs.lib.nixosSystem {
+        system = "x86_64-linux";
+        modules = [ ./nixos/configuration.moloch.nix ];
+      };
+    };
+  };
+}
