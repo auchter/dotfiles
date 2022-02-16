e1992f15272451037aabbadce199f64439876850
Michael Auchter <a@phire.org>
2022-02-16 15:53:04 +0000

    remove configs moved to home-manager

--- zsh/.zsh/functions.zsh
+++
@@ -1,7 +0,0 @@
-
-function weather {
-   loc=$1
-   [[ -z $loc ]] && loc=78756
-   # incredibly fragile, but why not?
-   hget 'http://thefuckingweather.com/?where='$loc | htmlfmt | sed -e '2d' -e '/^$/q'
-}
--- zsh/.zsh/grim.zsh
+++
@@ -1,7 +0,0 @@
-export GRIM_DEFAULT_DIR="$HOME/screenshots"
-
-if [ ! -d $GRIM_DEFAULT_DIR ]; then
-	mkdir -p $GRIM_DEFAULT_DIR
-fi
-
-alias sshot='grim -g "$(slurp)"'
