17935bb477c46cc6e6e1043c9d85ebad9ab5d95e
Michael Auchter <a@phire.org>
2022-02-21 16:06:41 +0000

    restart wpa_supplicant on config changes

--- nixos/modules/wifi.nix
+++ nixos/modules/wifi.nix
@@ -1,6 +1,9 @@
 { config, sops-nix, ... }:
 
 {
-  sops.secrets.wpa_supplicant.sopsFile = ../../secrets/wifi.yaml;
+  sops.secrets.wpa_supplicant = {
+    sopsFile = ../../secrets/wifi.yaml;
+    restartUnits = map (iface: "wpa_supplicant-" + iface) config.networking.wireless.interfaces;
+  };
   environment.etc."wpa_supplicant.conf".source = config.sops.secrets.wpa_supplicant.path;
 }
