4e916629d5dd068774ca5360d2cd2fea0071fe30
Michael Auchter <a@phire.org>
2022-06-21 23:43:33 +0000

    fixup! Add kde-connect

--- systems/moloch/kde-connect.nix
+++ systems/moloch/kde-connect.nix
@@ -6,12 +6,10 @@
   ];
 
   # Only open ports on the wireguard interface
-  networking.firewall.interfaces.wg0 = {
+  networking.firewall.interfaces.wg0 = rec {
     allowedUDPPortRanges = [
       { from = 1714; to = 1764; }
     ];
-    allowedTCPPortRanges = [
-      { from = 1714; to = 1764; }
-    ];
+    allowedTCPPortRanges = allowedUDPPortRanges;
   };
 }
