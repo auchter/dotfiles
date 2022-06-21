44eb75b19ac59e828390528a7794da8c641b6d3b
Michael Auchter <a@phire.org>
2022-06-21 23:29:36 +0000

    flake: update nixpkgs location

--- flake.nix
+++ flake.nix
@@ -1,6 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.05";
+    #nixpkgs.url = "path:/home/a/git/nixpkgs";
+    #nixpkgs.url = "nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:auchter/nixpkgs/auchter-22.05";
     home-manager.url = "github:rycee/home-manager/release-22.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
