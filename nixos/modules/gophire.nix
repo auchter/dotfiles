83b68dfa694ecf0a9135b9ce7e10171ff2f87f50
Michael Auchter <a@phire.org>
2022-06-23 22:15:23 +0000

    update

--- nixos/modules/gophire.nix
+++ nixos/modules/gophire.nix
@@ -2,16 +2,18 @@
 
 let
   sites = {
-    "hn" = "https://news.ycombinator.com";
-    "lo" = "https://liveoakbrewing.com/taproom/";
-    "ph" = "https://pinthouse.com/burnet/beer/beer-on-tap";
-    "zb" = "https://zilkerbeer.com/?v=47e5dceea252#seasonals-beer";
-    "sb" = "https://spicyboyschicken.square.site/?location=11eb52928bec852a8648ac1f6bbbd01e";
-    "ct" = "https://www.cuantostacosaustin.com/s/order";
-    "bh" = "https://www.betterhalfbar.com/menu";
-    "dh" = "https://www.draughthouse.com/drinks";
-    "wh" = "https://workhorsebar.e-tab.com/workhorsebar/venue/5ea0812fad8074513197d76a/menu";
-    "cf" = "https://chinafamilytx.com/menu/64123353";
+    hn = "https://news.ycombinator.com";
+    liveoak = "https://liveoakbrewing.com/taproom/";
+    pinthouse = "https://pinthouse.com/burnet/beer/beer-on-tap";
+    zilker = "https://zilkerbeer.com/?v=47e5dceea252#seasonals-beer";
+    spicyboys = "https://spicyboyschicken.square.site/?location=11eb52928bec852a8648ac1f6bbbd01e";
+    cuantos = "https://www.cuantostacosaustin.com/s/order";
+    betterhalf = "https://www.betterhalfbar.com/menu";
+    dh = "https://www.draughthouse.com/drinks";
+    workhorse = "https://workhorsebar.e-tab.com/workhorsebar/venue/5ea0812fad8074513197d76a/menu";
+    chinafamily = "https://chinafamilytx.com/menu/64123353";
+    holdout = "https://holdoutbrewing.com/menus";
+    tlocs = "https://tlocs-hotdogs-103892-107872.square.site/";
   };
 in
 {
