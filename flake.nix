de920881f2704271901a0526025aad2f1c6c49b4
Michael Auchter <a@phire.org>
2022-06-26 18:49:24 +0000

    update deps

--- flake.nix
+++ flake.nix
@@ -1,9 +1,7 @@
 {
   inputs = {
-    #nixpkgs.url = "path:/home/a/git/nixpkgs";
-    #nixpkgs.url = "nixpkgs/nixos-22.05";
     nixpkgs.url = "github:auchter/nixpkgs/auchter-22.05";
-    home-manager.url = "github:rycee/home-manager/release-22.05";
+    home-manager.url = "github:auchter/home-manager/auchter-22.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
   };
