61947a653459bef73b31c8a1a45a9d4a4cf612eb
Michael Auchter <a@phire.org>
2022-07-01 22:08:03 +0000

    waybar updates

--- users/modules/waybar.nix
+++ users/modules/waybar.nix
@@ -18,106 +18,111 @@
           "network"
           "pulseaudio"
           "cpu"
-          "disk"
           "memory"
+          "disk"
           "backlight"
-          "clock"
           "battery"
+          "clock"
         ];
 
         modules = {
-          "mpd" = {
-              "format" = "{stateIcon} {consumeIcon}{randomIcon}{repeatIcon}{singleIcon}{artist} - {album} - {title} ({elapsedTime:%M:%S}/{totalTime:%M:%S}) ⸨{songPosition}|{queueLength}⸩ {volume}% ";
-              "format-disconnected" = "Disconnected ";
-              "format-stopped" = "{consumeIcon}{randomIcon}{repeatIcon}{singleIcon}Stopped ";
-              "unknown-tag" = "N/A";
-              "interval" = 2;
-              "consume-icons" = {
-                  "on" = " ";
+          mpd = {
+              format = "{stateIcon} {consumeIcon}{randomIcon}{repeatIcon}{singleIcon}{artist} - {album} - {title} ({elapsedTime:%M:%S}/{totalTime:%M:%S}) ⸨{songPosition}|{queueLength}⸩ {volume}% ";
+              format-disconnected = "Disconnected ";
+              format-stopped = "{consumeIcon}{randomIcon}{repeatIcon}{singleIcon}Stopped ";
+              unknown-tag = "N/A";
+              interval = 2;
+              consume-icons = {
+                  on = " ";
               };
-              "random-icons" = {
-                  "off" = "<span color=\"#f53c3c\"></span> ";
-                  "on" = " ";
+              random-icons = {
+                  off = "<span color=\"#f53c3c\"></span> ";
+                  on = " ";
               };
-              "repeat-icons" = {
-                  "on" = " ";
+              repeat-icons = {
+                  on = " ";
               };
-              "single-icons" = {
-                  "on" = "1 ";
+              single-icons = {
+                  on = "1 ";
               };
-              "state-icons" = {
-                  "paused" = "";
-                  "playing" = "";
+              state-icons = {
+                  paused = "";
+                  playing = "";
               };
-              "tooltip-format" = "MPD (connected)";
-              "tooltip-format-disconnected" = "MPD (disconnected)";
+              tooltip-format = "MPD (connected)";
+              tooltip-format-disconnected = "MPD (disconnected)";
           };
-          "idle_inhibitor" = {
-              "format" = "{icon}";
-              "format-icons" = {
-                  "activated" = "";
-                  "deactivated" = "";
+          idle_inhibitor = {
+              format = "{icon}";
+              format-icons = {
+                  activated = "";
+                  deactivated = "";
               };
           };
-          "tray" = {
-              "spacing" = 10;
+          tray = {
+              spacing = 10;
           };
-          "clock" = {
-              "tooltip-format" = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
-              "format-alt" = "{:%Y-%m-%d}";
+          clock = {
+              tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+              format-alt = "{:%Y-%m-%d}";
+              format = "{:%Y-%m-%d %H:%M}";
+              timezone = "America/Chicago";
           };
-          "cpu" = {
-              "format" = "{usage}% ";
-              "tooltip" = false;
+          cpu = {
+              format = "{usage}% ";
+              tooltip = false;
           };
-          "memory" = {
-              "format" = "{}% ";
+          memory = {
+              format = "{}% ";
           };
-          "temperature" = {
-              "critical-threshold" = 80;
-              "format" = "{temperatureC}°C {icon}";
-              "format-icons" = ["" "" ""];
+          disk = {
+            format = "{percentage_used}% /";
           };
-          "backlight" = {
-              "format" = "{percent}% {icon}";
-              "format-icons" = ["" ""];
+          temperature = {
+              critical-threshold = 80;
+              format = "{temperatureC}°C {icon}";
+              format-icons = ["" "" ""];
           };
-          "battery" = {
-              "states" = {
-                  "warning" = 30;
-                  "critical" = 15;
+          backlight = {
+              format = "{percent}% {icon}";
+              format-icons = ["" ""];
+          };
+          battery = {
+              states = {
+                  warning = 30;
+                  critical = 15;
               };
-              "format" = "{capacity}% {icon}";
-              "format-charging" = "{capacity}% ";
-              "format-plugged" = "{capacity}% ";
-              "format-alt" = "{time} {icon}";
-              "format-icons" = ["" "" "" "" ""];
+              format = "{capacity}% {icon}";
+              format-charging = "{capacity}% ";
+              format-plugged = "{capacity}% ";
+              format-alt = "{time} {icon}";
+              format-icons = ["" "" "" "" ""];
           };
-          "network" = {
-              "format-wifi" = "{essid} ({signalStrength}%) ";
-              "format-ethernet" = "{ipaddr}/{cidr} ";
-              "tooltip-format" = "{ifname} via {gwaddr} ";
-              "format-linked" = "{ifname} (No IP) ";
-              "format-disconnected" = "Disconnected ⚠";
-              "format-alt" = "{ifname} = {ipaddr}/{cidr}";
+          network = {
+              format-wifi = "{essid} ({signalStrength}%) ";
+              format-ethernet = "{ipaddr}/{cidr} ";
+              tooltip-format = "{ifname} via {gwaddr} ";
+              format-linked = "{ifname} (No IP) ";
+              format-disconnected = "Disconnected ⚠";
+              format-alt = "{ifname} = {ipaddr}/{cidr}";
           };
-          "pulseaudio" = {
-              "format" = "{volume}% {icon} {format_source}";
-              "format-bluetooth" = "{volume}% {icon} {format_source}";
-              "format-bluetooth-muted" = " {icon} {format_source}";
-              "format-muted" = " {format_source}";
-              "format-source" = "{volume}% ";
-              "format-source-muted" = "";
-              "format-icons" = {
-                  "headphone" = "";
-                  "hands-free" = "";
-                  "headset" = "";
-                  "phone" = "";
-                  "portable" = "";
-                  "car" = "";
-                  "default" = ["" "" ""];
+          pulseaudio = {
+              format = "{volume}% {icon} {format_source}";
+              format-bluetooth = "{volume}% {icon} {format_source}";
+              format-bluetooth-muted = " {icon} {format_source}";
+              format-muted = " {format_source}";
+              format-source = "{volume}% ";
+              format-source-muted = "";
+              format-icons = {
+                  headphone = "";
+                  hands-free = "";
+                  headset = "";
+                  phone = "";
+                  portable = "";
+                  car = "";
+                  default = ["" "" ""];
               };
-              "on-click" = "pavucontrol";
+              on-click = "pavucontrol";
           };
         };
       };
