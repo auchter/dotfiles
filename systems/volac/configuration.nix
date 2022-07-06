2b25a886af6adac9c05d3c80833bc9c5cf775792
Michael Auchter <a@phire.org>
2022-07-06 09:47:22 -0400

    volac: update config

--- systems/volac/configuration.nix
+++ systems/volac/configuration.nix
@@ -4,6 +4,9 @@
   imports =
     [ 
       ./hardware-configuration.nix
+      ../../nixos/modules/base.nix
+      ../../nixos/modules/users.nix
+      ../../nixos/modules/acme.nix
     ];
 
   boot.loader.grub.enable = false;
@@ -20,24 +23,6 @@
       "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzIue421fOG3rtf/OrysL6lbGYuyavrZtQEPgXNkeYqQmBcHTGBujhXkr6fXVB+l4I2E+eWz+vyDD0JZWYZYeyyFSTS6yArIH+sEdEFzqNjQ+H0td5sEfysZfOkeGSKwNhnSKl5yVFkIn3DE3igCS97CqNBGf2kBLX//BGvgrvGLVvDIv6x0hUhNjHyTcth510sRi3TTcDb+GwNQqHyl9K/gtQFXVNVXg1bxGhzDD+NFARHiaPys6/QHP3N2X6KJV+1L3Of/w2+YLNPhRaJMBw3C4nrNoC/vPbH0m2pO3JhfILW7f7EgZSfp82vEO2XGuKbMc+1tXUaInA6Gy889f1 a@moloch.phire.org"
     ];
   };
-
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    git
-    htop
-    tmux
-    tmux
-    vim
-    wget
-  ];
-
-  services.openssh.enable = true;
-
-  nix.package = pkgs.nixUnstable;
-  nix.extraOptions = ''
-    experimental-features = nix-command flakes
-  '';
 
   system.stateVersion = "22.05"; # Did you read the comment?
 }
