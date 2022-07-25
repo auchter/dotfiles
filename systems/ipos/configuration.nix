315f61c3bf3a5e5d697c92a7cd84c4fa67fa3f5a
Michael Auchter <a@phire.org>
2022-07-25 15:34:43 +0000

    drop gotify

--- systems/ipos/configuration.nix
+++ systems/ipos/configuration.nix
@@ -7,7 +7,6 @@
       ../../nixos/modules/users.nix
       ../../nixos/modules/acme.nix
       ../../nixos/modules/miniflux.nix
-      ../../nixos/modules/gotify.nix
       ../../nixos/modules/gophire.nix
       ../../nixos/modules/sops.nix
       ../../nixos/modules/wg-server.nix
