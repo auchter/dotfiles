2cfd431219b73cbb5812718d0ed8cf11aff47cbf
Michael Auchter <a@phire.org>
2022-07-26 02:24:27 +0000

    fixup

--- systems/volac/configuration.nix
+++ systems/volac/configuration.nix
@@ -36,26 +36,6 @@
     configDir = "/var/lib/home-assistant";
   };
 
-  services.nginx = {
-    enable = true;
-    virtualHosts = {
-      "cottage.phire.org" = {
-        forceSSL = true;
-        enableACME = true;
-        locations."/" = {
-          proxyPass = "http://127.0.0.1:8123";
-          proxyWebsockets = true;
-          extraConfig =
-            "proxy_redirect off;" +
-            "proxy_set_header Host $host;" +
-            "proxy_set_header X-Real-IP $remote_addr;" +
-            "proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;" +
-            "proxy_set_header X-Forwarded-Proto $scheme;";
-        };
-      };
-    };
-  };
-
   # Temporary...
   users.users.nixos = {
     isNormalUser = true;
