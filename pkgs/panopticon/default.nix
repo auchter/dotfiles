0276caacffef80b444487a7bbcf8f07df2e171cd
Michael Auchter <a@phire.org>
2022-08-05 15:58:49 +0000

    pkgs: update panopticon

--- pkgs/panopticon/default.nix
+++ pkgs/panopticon/default.nix
@@ -11,8 +11,8 @@ python3Packages.buildPythonApplication rec {
   src = fetchFromGitHub {
     owner = "auchter";
     repo = pname;
-    rev = "4d084b5ebe75b61af9fec7ba2cada61f4da4d214";
-    sha256 = "sha256-1a+T3wnlppxgDIxw6i7OrvAisbjPMjdvHQgC4sXfsuY=";
+    rev = "31eda7118f0219b420da09340cb0a7f316b95feb";
+    sha256 = "sha256-/j5dtWNdDZmLBfferVkSOVIZOZ4PbXGtyUAR3PH+608=";
   };
 
   propagatedBuildInputs = with python3Packages; [
