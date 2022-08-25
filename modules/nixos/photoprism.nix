9c7126300475db4d5fbd88f245cc1cc1ba3e3c2c
Michael Auchter <a@phire.org>
2022-08-25 19:43:39 +0000

    update photoprism

--- modules/nixos/photoprism.nix
+++ modules/nixos/photoprism.nix
@@ -16,10 +16,29 @@ in {
     importDir = mkOption {
       description = "Folder from which files can be transferred to the originals folder in a structured way that avoids duplicates";
     };
+    vhost = mkOption { };
   };
 
   config = mkIf (cfg.enable) {
-    networking.firewall.allowedTCPPorts = [ 2342 ];
+
+    services.nginx = {
+      enable = true;
+      virtualHosts."${cfg.vhost}" = {
+        forceSSL = true;
+        enableACME = true;
+        locations."/" = {
+          proxyPass = "http://127.0.0.1:2342";
+          proxyWebsockets = true;
+          extraConfig =
+            "proxy_redirect off;" +
+            "proxy_set_header Host $host;" +
+            "proxy_set_header X-Real-IP $remote_addr;" +
+            "proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;" +
+            "proxy_set_header X-Forwarded-Proto $scheme;";
+        };
+      };
+    };
+
     virtualisation.oci-containers = {
       containers = {
         photoprism = {
@@ -31,7 +50,7 @@ in {
           ];
           ports = [ "2342:2342" ];
           environment = {
-            PHOTOPRISM_ADMIN_PASSWORD = "woopwoop";
+            PHOTOPRISM_ADMIN_PASSWORD = "changeme1234";
           };
         };
       };
