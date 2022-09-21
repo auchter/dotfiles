27be8fd1e013118afc1554a15c2c138261900fd6
Michael Auchter <a@phire.org>
2022-09-21 19:20:10 +0000

    malphas: remove dts patches

--- systems/malphas/configuration.nix
+++ systems/malphas/configuration.nix
@@ -10,20 +10,6 @@
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
 
-  boot.kernelPatches = [
-    {
-      name = "preamp dts";
-      patch = ./0001-arm64-dts-add-device-tree-for-preamp.patch;
-      extraConfig = ''
-        GPIO_PCF857X y
-      '';
-    }
-    { # really should just merge this into the above...
-      name = "gpio line names";
-      patch = ./0002-add-line-names.patch;
-    }
-  ];
-
   networking.interfaces.eth0.useDHCP = true;
   modules.sshd.enable = true;
 
