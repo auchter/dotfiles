3eb8df235918af836513ea0753c27eee44fc7fb4
Michael Auchter <a@phire.org>
2022-09-22 16:12:24 +0000

    patch up snapcast

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,5 +1,14 @@
 { nixpkgs, ... }:
 let
   additions = final: prev: import ../pkgs { pkgs = prev; };
+  snapcast = final: prev: {
+    snapcast = prev.snapcast.overrideAttrs (old: {
+      patches = (old.patches or []) ++ [
+        ./snapcast/0001-Hack-in-BruteFIR-support.patch
+        ./snapcast/0002-Add-brutefir_config-option.patch
+        ./snapcast/0003-include-missing-headers.patch
+      ];
+    });
+  };
 in
-  nixpkgs.lib.composeManyExtensions [ additions ]
+  nixpkgs.lib.composeManyExtensions [ additions snapcast ]
