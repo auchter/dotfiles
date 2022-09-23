cf8e6db30340b59acc2da9c668a2f81a77ac0cf3
Michael Auchter <a@phire.org>
2022-09-23 18:38:57 +0000

    malphas: fixup devicetree

--- systems/malphas/configuration.nix
+++ systems/malphas/configuration.nix
@@ -49,41 +49,42 @@
           fragment@0 {
             target = <&i2c0>;
             __overlay__ {
-             delta1_set: gpio@3f {
-               compatible = "nxp,pcf8574a";
-               reg = <0x3f>;
-               gpio-controller;
-               gpio-cells = <2>;
-               lines-initial-states = <0xF>;
-               gpio-line-names = "DELTA1_SET_0",
-                                 "DELTA1_SET_1",
-                                 "DELTA1_SET_2",
-                                 "DELTA1_SET_3",
-                                 "DELTA1_SET_4",
-                                 "DELTA1_SET_5",
-                                 "DELTA1_SET_6",
-                                 "DELTA1_SET_7";
-             };
+              status = "okay";
+              delta1_set: gpio@3f {
+                compatible = "nxp,pcf8574a";
+                reg = <0x3f>;
+                gpio-controller;
+                gpio-cells = <2>;
+                lines-initial-states = <0xF>;
+                gpio-line-names = "DELTA1_SET_0",
+                                  "DELTA1_SET_1",
+                                  "DELTA1_SET_2",
+                                  "DELTA1_SET_3",
+                                  "DELTA1_SET_4",
+                                  "DELTA1_SET_5",
+                                  "DELTA1_SET_6",
+                                  "DELTA1_SET_7";
+              };
 
-             delta1_rst: gpio@3e {
-               compatible = "nxp,pcf8574a";
-               reg = <0x3e>;
-               gpio-controller;
-               gpio-cells = <2>;
-               lines-initial-states = <0xF>;
-               gpio-line-names = "DELTA1_RST_0",
-                                 "DELTA1_RST_1",
-                                 "DELTA1_RST_2",
-                                 "DELTA1_RST_3",
-                                 "DELTA1_RST_4",
-                                 "DELTA1_RST_5",
-                                 "DELTA1_RST_6",
-                                 "DELTA1_RST_7";
-             };
-           };
-         };
-       };
-     '';
+              delta1_rst: gpio@3e {
+                compatible = "nxp,pcf8574a";
+                reg = <0x3e>;
+                gpio-controller;
+                gpio-cells = <2>;
+                lines-initial-states = <0xF>;
+                gpio-line-names = "DELTA1_RST_0",
+                                  "DELTA1_RST_1",
+                                  "DELTA1_RST_2",
+                                  "DELTA1_RST_3",
+                                  "DELTA1_RST_4",
+                                  "DELTA1_RST_5",
+                                  "DELTA1_RST_6",
+                                  "DELTA1_RST_7";
+              };
+            };
+          };
+        };
+      '';
     }
     {
       name = "delta2-overlay";
@@ -95,41 +96,42 @@
           fragment@0 {
             target = <&i2c0>;
             __overlay__ {
-             delta2_set: gpio@3d {
-               compatible = "nxp,pcf8574a";
-               reg = <0x3d>;
-               gpio-controller;
-               gpio-cells = <2>;
-               lines-initial-states = <0xF>;
-               gpio-line-names = "DELTA2_SET_0",
-                                 "DELTA2_SET_1",
-                                 "DELTA2_SET_2",
-                                 "DELTA2_SET_3",
-                                 "DELTA2_SET_4",
-                                 "DELTA2_SET_5",
-                                 "DELTA2_SET_6",
-                                 "DELTA2_SET_7";
-             };
+              status = "okay";
+              delta2_set: gpio@3d {
+                compatible = "nxp,pcf8574a";
+                reg = <0x3d>;
+                gpio-controller;
+                gpio-cells = <2>;
+                lines-initial-states = <0xF>;
+                gpio-line-names = "DELTA2_SET_0",
+                                  "DELTA2_SET_1",
+                                  "DELTA2_SET_2",
+                                  "DELTA2_SET_3",
+                                  "DELTA2_SET_4",
+                                  "DELTA2_SET_5",
+                                  "DELTA2_SET_6",
+                                  "DELTA2_SET_7";
+              };
 
-             delta2_rst: gpio@3c {
-               compatible = "nxp,pcf8574a";
-               reg = <0x3c>;
-               gpio-controller;
-               gpio-cells = <2>;
-               lines-initial-states = <0xF>;
-               gpio-line-names = "DELTA2_RST_0",
-                                 "DELTA2_RST_1",
-                                 "DELTA2_RST_2",
-                                 "DELTA2_RST_3",
-                                 "DELTA2_RST_4",
-                                 "DELTA2_RST_5",
-                                 "DELTA2_RST_6",
-                                 "DELTA2_RST_7";
-             };
-           };
-         };
-       };
-     '';
+              delta2_rst: gpio@3c {
+                compatible = "nxp,pcf8574a";
+                reg = <0x3c>;
+                gpio-controller;
+                gpio-cells = <2>;
+                lines-initial-states = <0xF>;
+                gpio-line-names = "DELTA2_RST_0",
+                                  "DELTA2_RST_1",
+                                  "DELTA2_RST_2",
+                                  "DELTA2_RST_3",
+                                  "DELTA2_RST_4",
+                                  "DELTA2_RST_5",
+                                  "DELTA2_RST_6",
+                                  "DELTA2_RST_7";
+              };
+            };
+          };
+        };
+      '';
     }
   ];
 
