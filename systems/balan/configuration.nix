d4fab065486c7e8f0f549da6de197b53a13d2539
Michael Auchter <a@phire.org>
2022-09-28 21:27:53 +0000

    balan: add snapclient and brutefir

--- systems/balan/configuration.nix
+++ systems/balan/configuration.nix
@@ -13,6 +13,41 @@
   networking.interfaces.eth0.useDHCP = true;
   modules.sshd.enable = true;
 
+  sound.enable = true;
+
+  modules.snapclient = {
+    enable = true;
+    host = "malphas";
+    sampleFormat = "44100:16:*";
+    soundcard = "front:CARD=adapter,DEV=0";
+  };
+
+  modules.brutefir = {
+    enable = true;
+    inputs = {
+      "in" = {
+        channels = 2;
+        format = "S16_LE";
+      };
+    };
+    outputs = {
+      out = {
+        channels = 2;
+        format = "S16_LE";
+      };
+    };
+    filters = {
+      filter = {
+        input = "in";
+        output = "out";
+      };
+    };
+    coeffs = {
+      dirac.path = "dirac pulse";
+      bed_door_open.path = ./brutefir/bed_door_open.wav;
+    };
+  };
+
   system.stateVersion = "22.05"; # Did you read the comment?
 }
 
