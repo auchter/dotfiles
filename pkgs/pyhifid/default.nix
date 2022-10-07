eb52b7f52863959e569162d8e760467940337c09
Michael Auchter <a@phire.org>
2022-10-07 22:27:28 +0000

    pkgs: pyhifid: bump it

--- pkgs/pyhifid/default.nix
+++ pkgs/pyhifid/default.nix
@@ -8,13 +8,13 @@
 
 python3Packages.buildPythonApplication rec {
   pname = "pyhifid";
-  version = "unstable-2022-10-06";
+  version = "unstable-2022-10-07";
 
   src = fetchFromGitHub {
     owner = "auchter";
     repo = pname;
-    rev = "dda1c2c36454ae83ef093d423f790c1c98806dc3";
-    sha256 = "sha256-+FvifHxnCCrCIkuNJV2Y7b7GEVt875fzeZEaddoBH/c=";
+    rev = "1fe6d5c142d5b8ac7bc7b80cc1f5cade7215243b";
+    sha256 = "sha256-gQKQDJrCVqoDeIhSenXg1ywcdd+j6dBHHdUEMM4qMqI=";
   };
 
   propagatedBuildInputs = with python3Packages; [
