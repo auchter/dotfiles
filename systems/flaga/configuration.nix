fa222c32044ea7006862243aca5106ff404c05d9
Michael Auchter <a@phire.org>
2022-10-16 19:14:53 +0000

    flaga: enable dhcp

--- systems/flaga/configuration.nix
+++ systems/flaga/configuration.nix
@@ -10,6 +10,7 @@
   boot.loader.grub.enable = false;
   boot.loader.generic-extlinux-compatible.enable = true;
 
+  networking.interfaces.eth0.useDHCP = true;
   modules.sshd.enable = true;
 
   system.stateVersion = "22.05"; # Did you read the comment?
