5e00581da1e4fd5e81e53158347670950b6cd104
Michael Auchter <a@phire.org>
2022-10-17 02:22:58 +0000

    flaga: home-assistant

--- systems/flaga/configuration.nix
+++ systems/flaga/configuration.nix
@@ -13,6 +13,35 @@
   networking.interfaces.eth0.useDHCP = true;
   modules.sshd.enable = true;
 
+  services.avahi = {
+    enable = true;
+    nssmdns = true;
+  };
+
+  services.home-assistant = {
+    enable = true;
+    extraComponents = [
+      "default_config"
+      "met"
+      "esphome"
+      "rpi_power"
+      "lifx"
+      "wled"
+      "zwave_js"
+    ];
+
+    config = {
+      homeassistant = {
+        name = "Home";
+        unit_system = "imperial";
+        time_zone = "America/Chicago";
+      };
+      default_config = {};
+    };
+
+    openFirewall = true;
+  };
+
   system.stateVersion = "22.05"; # Did you read the comment?
 }
 
