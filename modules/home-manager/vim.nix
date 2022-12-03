8aca787d899d95f6cc18c4f04b6323e046f08658
Michael Auchter <a@phire.org>
2022-12-03 14:56:20 +0000

    vim: update cpp

--- modules/home-manager/vim.nix
+++ modules/home-manager/vim.nix
@@ -82,7 +82,7 @@ in {
         au FileType java setl sw=4 sts=4 ts=4 noexpandtab
         au FileType python setl sw=4 sts=4 ts=4 expandtab
         au FileType haskell setl sw=4 sts=4 ts=8 expandtab
-        " au FileType cpp setl sw=4 sts=4 ts=4 expandtab
+        au FileType cpp setl sw=2 sts=2 ts=2 expandtab
         au FileType cmake setl sw=4 sts=4 ts=4 expandtab
         au FileType yaml setlocal ts=2 sts=2 sw=2 expandtab
 
