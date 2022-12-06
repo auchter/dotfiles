32cb51b0a8e8605a3f4339bd9977dbfe87b6b187
Michael Auchter <a@phire.org>
2022-12-06 23:45:24 +0000

    andras: add brutefir config

--- systems/andras/configuration.nix
+++ systems/andras/configuration.nix
@@ -18,6 +18,44 @@
     interfaces = [ "wlan0" ];
   };
 
+  sound.enable = true;
+
+  modules.snapclient = {
+    enable = true;
+    host = "malphas";
+    sampleFormat = "44100:16:*";
+    soundcard = "front:CARD=Audio,DEV=0";
+  };
+
+  modules.brutefir = {
+    enable = true;
+    inputs = {
+      "in" = {
+        channels = 2;
+        format = "S16_LE";
+      };
+    };
+    outputs = {
+      out = {
+        channels = 2;
+        format = "S16_LE";
+      };
+    };
+    filters = {
+      filter = {
+        input = "in";
+        output = "out";
+        coeff = "bathroom";
+      };
+    };
+    coeffs = {
+      dirac.path = "dirac pulse";
+      bathroom = {
+        path = ./brutefir/bathroom.wav;
+      };
+    };
+  };
+
   system.stateVersion = "22.05"; # Did you read the comment?
 }
 
