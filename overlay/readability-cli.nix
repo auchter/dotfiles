c6c58b13982537af558a1c9b6158c97ccb46029a
Michael Auchter <a@phire.org>
2022-12-10 17:20:41 +0000

    overlay: add temporary overlay for readability-cli
    From https://github.com/NixOS/nixpkgs/pull/202100, can be dropped once
    merged.

--- overlay/readability-cli.nix
+++ overlay/readability-cli.nix
@@ -1,4 +1,22 @@
 { prev, final }:
 {
+ nodePackages.readability-cli = prev.nodePackages.readability-cli.override (oldAttrs: {
+    # Wrap src to fix this build error:
+    # > readability-cli/readable.ts: unsupported interpreter directive "#!/usr/bin/env -S deno..."
+    #
+    # Need to wrap the source, instead of patching in patchPhase, because
+    # buildNodePackage only unpacks sources in the installPhase.
+    src = final.srcOnly {
+      src = oldAttrs.src;
+      name = oldAttrs.name;
+      patchPhase = "chmod a-x readable.ts";
+    };
 
+    nativeBuildInputs = [ final.pkg-config ];
+    buildInputs = with final; [
+      pixman
+      cairo
+      pango
+    ];
+  });
 }
