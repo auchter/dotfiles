62d4b1dd7805e9b5f352211002eb0ea37b4e865f
Michael Auchter <a@phire.org>
2022-12-16 15:26:17 +0000

    sort overlays

--- overlay/default.nix
+++ overlay/default.nix
@@ -1,13 +1,13 @@
 { nixpkgs, ... }:
 let
   additions = _final: prev: import ../pkgs { pkgs = prev; };
-  snapcast = final: prev: import ./snapcast { inherit final; inherit prev; };
-  raspberrypiWirelessFirmware = final: prev: import ./raspberrypi-wireless { inherit final; inherit prev; };
   nodePackages.readability-cli = final: prev: import ./readability-cli.nix { inherit final; inherit prev; };
+  raspberrypiWirelessFirmware = final: prev: import ./raspberrypi-wireless { inherit final; inherit prev; };
+  snapcast = final: prev: import ./snapcast { inherit final; inherit prev; };
 in
   nixpkgs.lib.composeManyExtensions [
     additions
-    snapcast
-    raspberrypiWirelessFirmware
     nodePackages.readability-cli
+    raspberrypiWirelessFirmware
+    snapcast
   ]
