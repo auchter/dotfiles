0cd0e3b3e9955ab39816fccba584e63b65e67888
Michael Auchter <a@phire.org>
2023-01-01 17:24:53 +0000

    pkgs: beets-rym: point towards github

--- pkgs/beets-rym/default.nix
+++ pkgs/beets-rym/default.nix
@@ -8,7 +8,12 @@ python3Packages.buildPythonApplication rec {
   pname = "beets-rym";
   version = "unstable-2022-08-15";
 
-  src = /home/a/git/beets-rym;
+  src = fetchFromGitHub {
+    owner = "auchter";
+    repo = pname;
+    rev = "87b249dc42cd90103673f5dec275074521b135ba";
+    sha256 = "sha256-Rg4x6Jjd9xoBAz0IBEw4fgGVksEW2gnkCEdPWH3LuM4=";
+  };
 
   nativeBuildInputs = [ beets ];
 
