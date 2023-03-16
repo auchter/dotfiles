2dd549ef8cf8621c1f343b97f644af4aab5dbed4
Michael Auchter <a@phire.org>
2023-03-16 13:41:53 +0000

    fixup listenbrainz-mpd

--- modules/nixos/listenbrainz-mpd.nix
+++ modules/nixos/listenbrainz-mpd.nix
@@ -30,24 +30,30 @@ in {
     systemd.services.listenbrainz-mpd = {
       description = "listenbrainz-mpd client";
       after = [ "network.target" "mpd.service" ];
-      wantedBy = [ "mpd.service" ];
+      wantedBy = [ "default.target" ];
       serviceConfig = {
         LoadCredential = "token:${cfg.tokenFile}";
         Environment = [
-          "XDG_CONFIG_DIR=/run/listenbrainz-mpd"
+          "HOME=/run/listenbrainz-mpd"
+          "XDG_CONFIG_DIR=/run/listenbrainz-mpd/.config"
         ];
-        ExecStartPre = pkgs.writeShellScript lb-mpd ''
-          mkdir -p /run/listenbrainz-mpd/listenbrainz-mpd
-          TOKEN=$(cat ''${CREDENTIALS_DIRECTORY}/token)
-          cat <<EOF >/run/listenbrainz-mpd/listenbrainz-mpd/config.toml
+        ExecStart = pkgs.writeShellScript "lb-mpd" ''
+          echo HOME: $HOME
+          echo XDG_CONFIG_DIR: $XDG_CONFIG_DIR
+          mkdir -p $XDG_CONFIG_DIR/listenbrainz-mpd
+          echo CRED: $CREDENTIALS_DIRECTORY
+          ls -lsa $CREDENTIALS_DIRECTORY/..
+          ls -lsa $CREDENTIALS_DIRECTORY
+          TOKEN=$(cat $CREDENTIALS_DIRECTORY/token)
+          cat <<EOF >$XDG_CONFIG_DIR/listenbrainz-mpd/config.toml
           [submission]
           token = "$TOKEN"
 
           [mpd]
           address = "${cfg.mpdHost}"
           EOF
+          ${cfg.package}/bin/listenbrainz-mpd
         '';
-        ExecStart = "${cfg.package}/bin/listenbrainz-mpd";
         Type = "simple";
         Restart = "always";
         DynamicUser = true;
