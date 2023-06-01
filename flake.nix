5d758e1d4a8cee6560736832a5cf6163424a5349
Michael Auchter <a@phire.org>
2023-06-01 23:21:59 +0000

    nixos-23.05: stoat
    I aint no pole cat
    I aint no stoat
    I aint no rat
    This hole aint me i aint going down it
    I aint no ferrit
    Goin down an hole

--- flake.nix
+++ flake.nix
@@ -1,8 +1,8 @@
 {
   inputs = {
-    nixpkgs.url = "nixpkgs/nixos-22.11";
+    nixpkgs.url = "nixpkgs/nixos-23.05";
     #nixpkgs.url = "path:/home/a/git/nixpkgs";
-    home-manager.url = "github:rycee/home-manager/release-22.11";
+    home-manager.url = "github:rycee/home-manager/release-23.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     sops-nix.url = "github:Mic92/sops-nix";
     rust-overlay.url = "github:oxalica/rust-overlay";
