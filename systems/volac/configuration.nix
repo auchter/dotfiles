1bd5690a70934ef519c5dd489c95da57ab8512ef
Michael Auchter <a@phire.org>
2023-07-06 01:01:13 +0000

    remove user

--- systems/volac/configuration.nix
+++ systems/volac/configuration.nix
@@ -83,15 +83,6 @@
     vhost = "caseville.phire.org";
   };
 
-  # Temporary...
-  users.users.nixos = {
-    isNormalUser = true;
-    extraGroups = [ "wheel" ];
-    openssh.authorizedKeys.keys = [
-      "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQCzIue421fOG3rtf/OrysL6lbGYuyavrZtQEPgXNkeYqQmBcHTGBujhXkr6fXVB+l4I2E+eWz+vyDD0JZWYZYeyyFSTS6yArIH+sEdEFzqNjQ+H0td5sEfysZfOkeGSKwNhnSKl5yVFkIn3DE3igCS97CqNBGf2kBLX//BGvgrvGLVvDIv6x0hUhNjHyTcth510sRi3TTcDb+GwNQqHyl9K/gtQFXVNVXg1bxGhzDD+NFARHiaPys6/QHP3N2X6KJV+1L3Of/w2+YLNPhRaJMBw3C4nrNoC/vPbH0m2pO3JhfILW7f7EgZSfp82vEO2XGuKbMc+1tXUaInA6Gy889f1 a@moloch.phire.org"
-    ];
-  };
-
   modules.logo-site.logo = ../../logos/volac.png;
 
   system.stateVersion = "23.05"; # Did you read the comment?
