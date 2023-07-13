5e7528fb42f60bdd150b48e33744ba384b5b7070
Michael Auchter <a@phire.org>
2023-07-13 03:08:42 +0000

    kill snapcast overlay

--- overlay/default.nix
+++ overlay/default.nix
@@ -4,12 +4,10 @@ let
   mopidy-subidy = final: prev: import ./mopidy-subidy.nix { inherit final; inherit prev; };
   nodePackages.readability-cli = final: prev: import ./readability-cli.nix { inherit final; inherit prev; };
   raspberrypiWirelessFirmware = final: prev: import ./raspberrypi-wireless { inherit final; inherit prev; };
-  snapcast = final: prev: import ./snapcast { inherit final; inherit prev; };
 in
   nixpkgs.lib.composeManyExtensions [
     additions
     mopidy-subidy
     nodePackages.readability-cli
     raspberrypiWirelessFirmware
-    snapcast
   ]
