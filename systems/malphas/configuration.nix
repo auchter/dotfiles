609343f21b81454ade9afab6c885db13b65ad045
Michael Auchter <a@phire.org>
2023-08-13 19:34:05 +0000

    fixup! drop unused overlay

--- systems/malphas/configuration.nix
+++ systems/malphas/configuration.nix
@@ -289,53 +289,6 @@
         };
       '';
     }
-    {
-      name = "delta1-overlay";
-      dtsText = ''
-        /dts-v1/;
-        /plugin/;
-        / {
-          compatible = "pine64,pine-h64-model-b";
-          fragment@0 {
-            target = <&i2c0>;
-            __overlay__ {
-              status = "okay";
-              delta1_set: gpio@3f {
-                compatible = "nxp,pcf8574a";
-                reg = <0x3f>;
-                gpio-controller;
-                gpio-cells = <2>;
-                lines-initial-states = <0xF>;
-                gpio-line-names = "DELTA1_SET_0",
-                                  "DELTA1_SET_1",
-                                  "DELTA1_SET_2",
-                                  "DELTA1_SET_3",
-                                  "DELTA1_SET_4",
-                                  "DELTA1_SET_5",
-                                  "DELTA1_SET_6",
-                                  "DELTA1_SET_7";
-              };
-
-              delta1_rst: gpio@3e {
-                compatible = "nxp,pcf8574a";
-                reg = <0x3e>;
-                gpio-controller;
-                gpio-cells = <2>;
-                lines-initial-states = <0xF>;
-                gpio-line-names = "DELTA1_RST_0",
-                                  "DELTA1_RST_1",
-                                  "DELTA1_RST_2",
-                                  "DELTA1_RST_3",
-                                  "DELTA1_RST_4",
-                                  "DELTA1_RST_5",
-                                  "DELTA1_RST_6",
-                                  "DELTA1_RST_7";
-              };
-            };
-          };
-        };
-      '';
-    }
   ];
 
   system.stateVersion = "22.05"; # Did you read the comment?
