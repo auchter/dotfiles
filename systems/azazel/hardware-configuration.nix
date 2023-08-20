2d6ddb87dfb400a323f034926416a18e08a5b967
Michael Auchter <a@phire.org>
2023-08-20 14:21:38 +0000

    azazel: performance governor

--- systems/azazel/hardware-configuration.nix
+++ systems/azazel/hardware-configuration.nix
@@ -33,6 +33,6 @@
   # networking.interfaces.enp0s31f6.useDHCP = lib.mkDefault true;
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  powerManagement.cpuFreqGovernor = lib.mkDefault "powersave";
+  powerManagement.cpuFreqGovernor = lib.mkDefault "performance";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
 }
