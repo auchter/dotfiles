73d6e9b96d5f2c01c2e1973f44b82db79f46c038
Michael Auchter <a@phire.org>
2023-08-26 21:15:49 +0000

    setup new drive

--- systems/azazel/hardware-configuration.nix
+++ systems/azazel/hardware-configuration.nix
@@ -13,15 +13,38 @@
   boot.kernelModules = [ ];
   boot.extraModulePackages = [ ];
 
-  fileSystems."/" =
-    { device = "/dev/disk/by-uuid/c396a021-4bf8-4590-b1ee-b2a01603219c";
-      fsType = "ext4";
-    };
+  fileSystems."/" = {
+    device = "/dev/disk/by-uuid/c396a021-4bf8-4590-b1ee-b2a01603219c";
+    fsType = "ext4";
+  };
 
-  fileSystems."/boot" =
-    { device = "/dev/disk/by-uuid/988D-7431";
-      fsType = "vfat";
-    };
+  fileSystems."/boot" = {
+    device = "/dev/disk/by-uuid/988D-7431";
+    fsType = "vfat";
+  };
+
+  fileSystems."/mnt/unenc" = {
+    device = "/dev/disk/by-label/storage-unenc";
+    fsType = "ext4";
+    options = [ "noatime" ];
+  };
+
+  fileSystems."/mnt/storage" = {
+    device = "UUID=0789211e-1c89-442a-8372-d66172a9fe0c";
+    fsType = "ext4";
+    options = [ "noauto" "noatime" ];
+  };
+
+  environment.etc.crypttab = {
+    enable = true;
+    text = ''
+      storage_enc UUID=6d1c1ba4-62d5-4f46-a93a-e627fd186b35 none luks,noauto
+    '';
+  };
+
+  environment.systemPackages = with pkgs; [
+    cryptsetup
+  ];
 
   swapDevices = [ ];
 
