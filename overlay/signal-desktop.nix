f1a0eae3ced559ae7c6c29b748a9caf6dd008b7b
Michael Auchter <a@phire.org>
2023-08-27 03:01:38 +0000

    fixup! signal-desktop: none more black

--- overlay/signal-desktop.nix
+++ overlay/signal-desktop.nix
@@ -8,7 +8,9 @@
     postInstall = ''
       asar extract $out/lib/Signal/resources/app.asar "$TMP/sigwork"
       substituteInPlace "$TMP/sigwork/stylesheets/manifest.css" \
-        --replace "background-color: #121212;" "background-color: #000000;"
+        --replace "background-color: #121212;" "background-color: #000000;" \
+        --replace "background-color: #2e2e2e;" "background-color: #000000;" \
+        --replace "background-color: #3b3b3b;" "background-color: #000000;"
       asar pack "$TMP/sigwork" $out/lib/Signal/resources/app.asar
     '';
   });
