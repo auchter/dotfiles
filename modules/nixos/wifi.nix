467c6de040df2eabff56854197ba880d1c9e4fb3
Michael Auchter <a@phire.org>
2023-10-24 01:36:35 +0000

    Allow filtering which wifi networks are configured

--- modules/nixos/wifi.nix
+++ modules/nixos/wifi.nix
@@ -11,16 +11,23 @@ in {
       default = [];
       type = types.listOf types.str;
     };
+    networks = mkOption {
+      default = null;
+      type = types.nullOr (types.listOf types.str);
+      description = "which SSIDs should be included";
+    };
   };
 
   config = mkIf cfg.enable {
     networking.interfaces = builtins.listToAttrs (builtins.map (i: { name = i; value = { useDHCP = true; }; }) cfg.interfaces);
 
-    networking.wireless = {
+    networking.wireless = let
+      netPred = if (cfg.networks == null) then (n: v: true) else (n: v: elem n cfg.networks);
+    in {
       enable = true;
       interfaces = cfg.interfaces;
       environmentFile = config.sops.secrets.wpa_supplicant.path;
-      networks = import ./wifi.secret.nix;
+      networks = filterAttrs netPred (import ./wifi.secret.nix);
       userControlled = {
         enable = true;
         group = "wheel";
