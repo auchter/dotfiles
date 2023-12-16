9af89a6455d5f5b97d9b046d1d605dc02e069f2a
Michael Auchter <a@phire.org>
2023-12-16 15:04:42 +0000

    azazel now owns music.

--- systems/azazel/configuration.nix
+++ systems/azazel/configuration.nix
@@ -1,4 +1,4 @@
-{ config, ... }:
+{ config, pkgs, ... }:
 
 {
   imports =
@@ -24,9 +24,241 @@
 
   services.mpdpower = {
     enable = true;
-    mpdHost = "malphas";
+    mpdHost = "localhost";
     btAddr = "00:12:92:08:07:B9";
   };
+
+  # stereo config
+
+  services.mpd = {
+    enable = true;
+    musicDirectory = "/mnt/ssd/music";
+    playlistDirectory = "/mnt/ssd/music/playlists";
+    network = {
+      listenAddress = "any";
+    };
+    extraConfig = ''
+      resampler {
+        plugin "libsamplerate"
+        type "0"
+      }
+
+      auto_update "yes"
+      replaygain "auto"
+      audio_output {
+        type    "fifo"
+        name    "snapcast"
+        path    "/run/snapserver/mpd"
+        format  "44100:16:2"
+        mixer_type "null"
+      }
+      audio_output {
+        type   "null"
+        name   "CamillaDSP"
+        #device "hw:Loopback,0"
+        #format "96000:32:2"
+        mixer_type "null"
+      }
+      audio_output {
+        type "pipewire"
+        name "Pipewire"
+        mixer_type "null"
+      }
+    '';
+  };
+
+  systemd.services.mpd.serviceConfig.restart = "always";
+
+  modules.upmpdcli = {
+    enable = true;
+  };
+
+  services.snapserver = {
+    enable = true;
+    openFirewall = true;
+    http.docRoot = "${pkgs.snapcast}/share/snapserver/snapweb";
+    streams = {
+      mpd = {
+        type = "pipe";
+        location = "/run/snapserver/mpd";
+        query = {
+          mode = "create";
+          sampleformat = "44100:16:2";
+          codec = "ogg";
+        };
+      };
+    };
+  };
+
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    pulse.enable = true;
+    systemWide = true;
+  };
+
+  environment.etc = {
+    "pipewire/pipewire.conf.d/pipewire.conf".text = ''
+      context.properties = {
+        default.clock.rate = 96000
+      }
+      context.objects = [
+	{ factory = adapter
+          args = {
+              factory.name     = api.alsa.pcm.sink
+              node.name        = "camilladsp-sink"
+              node.description = "Alsa Loopback"
+              media.class      = "Audio/Sink"
+              api.alsa.path    = "hw:Loopback,0,0"
+              audio.format     = "S32LE"
+              audio.rate       = 96000
+              audio.channels   = 2
+              priority.session = 1400
+          }
+	}
+      ]
+    '';
+    "wireplumber/main.lua.d/81-stereo.lua".text = ''
+    '';
+  };
+
+  systemd.services.pipewire.restartTriggers = [
+    config.environment.etc."pipewire/pipewire.conf.d/pipewire.conf".text
+  ];
+
+  users.users.mpd.extraGroups = [ "audio" "pipewire" ];
+
+  boot.kernelModules = [ "snd-aloop" ];
+
+  services.mpdcamillamixer.enable = true;
+
+  services.camillagui = {
+    enable = true;
+    openFirewall = true;
+    port = 5001;
+  };
+
+  services.camilladsp = {
+    enable = true;
+    extraFilters = [
+    ];
+    config = let
+      mixCopy = dest: channel: {
+        inherit dest;
+        sources = [ { inherit channel; } ];
+      };
+      mixMono = dest: src1: src2: {
+        inherit dest;
+        sources = map (channel: { inherit channel; gain = -6; }) [src1 src2];
+      };
+      mkBiquad = type: params: {
+        type = "Biquad";
+        parameters = { inherit type; } // params;
+      };
+      mkBiquadCombo = type: freq: order: {
+        type = "BiquadCombo";
+        parameters = {
+          inherit type;
+          inherit freq;
+          inherit order;
+        };
+      };
+      mkPipelineFilter = channels: filters: map (channel: {
+        type = "Filter";
+        inherit channel;
+        names = filters;
+      }) channels;
+      mkPipelineMixer = name: [{
+        type = "Mixer";
+        inherit name;
+      }];
+    in {
+      devices = {
+        samplerate = 96000;
+        chunksize = 4096;
+        capture = {
+          type = "Alsa";
+          channels = 2;
+          device = "hw:Loopback,1";
+          format = "S32LE";
+        };
+        playback = {
+          type = "Alsa";
+          channels = 4;
+          device = "surround40:CARD=M4,DEV=0";
+          format = "S32LE";
+        };
+      };
+      mixers = {
+        main = {
+          channels = {
+            "in" = 2;
+            "out" = 4;
+          };
+          mapping = [
+            (mixCopy 0 0)
+            (mixCopy 1 1)
+            (mixCopy 2 0)
+            (mixCopy 3 1)
+            #(mixMono 2 0 1)
+            #(mixMono 3 0 1)
+          ];
+        };
+      };
+      filters = {
+        right_plugged_0 = mkBiquad "Peaking" { freq = 103.0; gain = -5.6; q = 3.818; };
+        right_plugged_1 = mkBiquad "Peaking" { freq = 219.0; gain = -6.4; q = 4.693; };
+        right_plugged_2 = mkBiquad "Peaking" { freq = 480.0; gain = -4.8; q = 1.203; };
+
+        left_plugged_0 = mkBiquad "Peaking" { freq = 102.0; gain = -3.5; q = 4.998; };
+        left_plugged_1 = mkBiquad "Peaking" { freq = 497; gain = -6.2; q = 2.590; };
+
+        right_unplugged_0 = mkBiquad "Peaking" { freq = 49.35; gain = -3.5; q = 4.618; };
+        right_unplugged_1 = mkBiquad "Peaking" { freq = 102.5; gain = -7; q = 3.442; };
+        right_unplugged_2 = mkBiquad "Peaking" { freq = 219.0; gain = -6.3; q = 4.872; };
+        right_unplugged_3 = mkBiquad "Peaking" { freq = 475.0; gain = -4.9; q = 1.259; };
+
+        left_unplugged_0 = mkBiquad "Peaking" { freq = 65.7; gain = -4.5; q = 2.081; };
+        left_unplugged_1 = mkBiquad "Peaking" { freq = 104.0; gain = -5.9; q = 4.067; };
+        left_unplugged_2 = mkBiquad "Peaking" { freq = 497.0; gain = -7.4; q = 1.895; };
+
+        highpass = mkBiquadCombo "LinkwitzRileyHighpass" 100 8;
+        lowpass = mkBiquadCombo "LinkwitzRileyLowpass" 100 8;
+        sub_l_0 = mkBiquad "Peaking" { freq = 43.6; gain = -4.6; q = 2.781; };
+        sub_l_1 = mkBiquad "Peaking" { freq = 81.9; gain = -5.5; q = 1.498; };
+        sub_r_0 = mkBiquad "Peaking" { freq = 49.75; gain = -4.0; q = 3.22; };
+        sub_r_1 = mkBiquad "Peaking" { freq = 90.1; gain = -8.4; q = 1.0; };
+      };
+      pipeline = builtins.concatLists [
+        (mkPipelineMixer "main")
+        (mkPipelineFilter [0] [
+          "highpass"
+          #"left_unplugged_0"
+          #"left_unplugged_1"
+          #"left_unplugged_2"
+        ])
+        (mkPipelineFilter [1] [
+          "highpass"
+          #"right_unplugged_0"
+          #"right_unplugged_1"
+          #"right_unplugged_2"
+          #"right_unplugged_3"
+        ])
+        (mkPipelineFilter [2] [
+          "lowpass"
+          "sub_l_0"
+          "sub_l_1"
+        ])
+        (mkPipelineFilter [3] [
+          "lowpass"
+          "sub_r_0"
+          "sub_r_1"
+        ])
+      ];
+    };
+  };
+
+  # end stereo config
 
   services.nginx = {
     enable = true;
@@ -49,6 +281,7 @@
   };
 
   networking.firewall.allowedTCPPorts = [
+    config.services.mpd.network.port
     80 443 # nginx
   ];
 
--- systems/malphas/configuration.nix
+++ systems/malphas/configuration.nix
@@ -46,58 +46,8 @@
   };
 
   system.activationScripts.irq-affinity = ''
-    echo e > /proc/irq/39/smp_affinity
+    echo e > /proc/irq/240/smp_affinity
   '';
-
-  services.mpd = {
-    enable = true;
-    dataDir = "/mnt/storage/mpd";
-    musicDirectory = "/mnt/storage/music";
-    playlistDirectory = "/mnt/storage/music/playlists";
-    network = {
-      listenAddress = "any";
-    };
-    extraConfig = ''
-      resampler {
-        plugin "libsamplerate"
-        type "2"
-      }
-
-      auto_update "yes"
-      replaygain "auto"
-      audio_output {
-        type    "fifo"
-        name    "snapcast"
-        path    "/run/snapserver/mpd"
-        format  "44100:16:2"
-        mixer_type "null"
-      }
-      audio_output {
-        type   "null"
-        name   "CamillaDSP"
-        #device "hw:Loopback,0"
-        #format "96000:32:2"
-        mixer_type "null"
-      }
-      audio_output {
-        type "pipewire"
-        name "Pipewire"
-        mixer_type "null"
-      }
-    '';
-  };
-
-  modules.upmpdcli = {
-    enable = true;
-  };
-
-  services.mympd = {
-    enable = false;
-    openFirewall = true;
-    logLevel = 3; # TODO: currently ignored, since mympd is being built in release mode
-    mpdHost = "localhost";
-    ssl = false;
-  };
 
   environment.systemPackages = with pkgs; [
     camilladsp
@@ -114,195 +64,22 @@
   services.listenbrainz-mpd = {
     enable = true;
     tokenFile = config.sops.secrets.listenbrainz_pass.path;
+    mpdHost = "azazel.local.phire.org";
   };
 
-  services.snapserver = {
+  services.ampctrl = {
     enable = true;
-    openFirewall = true;
-    http.docRoot = "${pkgs.snapcast}/share/snapserver/snapweb";
-    streams = {
-      mpd = {
-        type = "pipe";
-        location = "/run/snapserver/mpd";
-        query = {
-          mode = "create";
-          sampleformat = "44100:16:2";
-          codec = "ogg";
-        };
-      };
-    };
+    mpdHost = "azazel.local.phire.org";
   };
 
-  services.ampctrl.enable = true;
-  services.ttctrl.enable = true;
-  services.mpdcamillamixer.enable = true;
-
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    pulse.enable = true;
-    systemWide = true;
+  services.ttctrl = {
+    enable = false;
+    mpdHost = "azazel.local.phire.org";
   };
-
-  environment.etc = {
-    "pipewire/pipewire.conf.d/pipewire.conf".text = ''
-      context.properties = {
-        default.clock.rate = 96000
-      }
-      context.objects = [
-	{ factory = adapter
-          args = {
-              factory.name     = api.alsa.pcm.sink
-              node.name        = "camilladsp-sink"
-              node.description = "Alsa Loopback"
-              media.class      = "Audio/Sink"
-              api.alsa.path    = "hw:Loopback,0,0"
-              audio.format     = "S32LE"
-              audio.rate       = 96000
-              audio.channels   = 2
-              priority.session = 1400
-          }
-	}
-      ]
-    '';
-    "wireplumber/main.lua.d/81-stereo.lua".text = ''
-    '';
-  };
-
-  systemd.services.pipewire.restartTriggers = [
-    config.environment.etc."pipewire/pipewire.conf.d/pipewire.conf".text
-  ];
-
-  users.users.mpd.extraGroups = [ "audio" "pipewire" ];
 
   boot.kernelModules = [ "snd-aloop" ];
   sound = {
     enable = true;
-  };
-
-  services.camillagui = {
-    enable = true;
-    openFirewall = true;
-  };
-
-  services.camilladsp = {
-    enable = true;
-    extraFilters = [
-    ];
-    config = let
-      mixCopy = dest: channel: {
-        inherit dest;
-        sources = [ { inherit channel; } ];
-      };
-      mixMono = dest: src1: src2: {
-        inherit dest;
-        sources = map (channel: { inherit channel; gain = -6; }) [src1 src2];
-      };
-      mkBiquad = type: params: {
-        type = "Biquad";
-        parameters = { inherit type; } // params;
-      };
-      mkBiquadCombo = type: freq: order: {
-        type = "BiquadCombo";
-        parameters = {
-          inherit type;
-          inherit freq;
-          inherit order;
-        };
-      };
-      mkPipelineFilter = channels: filters: map (channel: {
-        type = "Filter";
-        inherit channel;
-        names = filters;
-      }) channels;
-      mkPipelineMixer = name: [{
-        type = "Mixer";
-        inherit name;
-      }];
-    in {
-      devices = {
-        samplerate = 96000;
-        chunksize = 4096;
-        capture = {
-          type = "Alsa";
-          channels = 2;
-          device = "hw:Loopback,1";
-          format = "S32LE";
-        };
-        playback = {
-          type = "Alsa";
-          channels = 4;
-          device = "surround40:CARD=M4,DEV=0";
-          format = "S32LE";
-        };
-      };
-      mixers = {
-        main = {
-          channels = {
-            "in" = 2;
-            "out" = 4;
-          };
-          mapping = [
-            (mixCopy 0 0)
-            (mixCopy 1 1)
-            (mixCopy 2 0)
-            (mixCopy 3 1)
-            #(mixMono 2 0 1)
-            #(mixMono 3 0 1)
-          ];
-        };
-      };
-      filters = {
-        right_plugged_0 = mkBiquad "Peaking" { freq = 103.0; gain = -5.6; q = 3.818; };
-        right_plugged_1 = mkBiquad "Peaking" { freq = 219.0; gain = -6.4; q = 4.693; };
-        right_plugged_2 = mkBiquad "Peaking" { freq = 480.0; gain = -4.8; q = 1.203; };
-
-        left_plugged_0 = mkBiquad "Peaking" { freq = 102.0; gain = -3.5; q = 4.998; };
-        left_plugged_1 = mkBiquad "Peaking" { freq = 497; gain = -6.2; q = 2.590; };
-
-        right_unplugged_0 = mkBiquad "Peaking" { freq = 49.35; gain = -3.5; q = 4.618; };
-        right_unplugged_1 = mkBiquad "Peaking" { freq = 102.5; gain = -7; q = 3.442; };
-        right_unplugged_2 = mkBiquad "Peaking" { freq = 219.0; gain = -6.3; q = 4.872; };
-        right_unplugged_3 = mkBiquad "Peaking" { freq = 475.0; gain = -4.9; q = 1.259; };
-
-        left_unplugged_0 = mkBiquad "Peaking" { freq = 65.7; gain = -4.5; q = 2.081; };
-        left_unplugged_1 = mkBiquad "Peaking" { freq = 104.0; gain = -5.9; q = 4.067; };
-        left_unplugged_2 = mkBiquad "Peaking" { freq = 497.0; gain = -7.4; q = 1.895; };
-
-        highpass = mkBiquadCombo "LinkwitzRileyHighpass" 100 8;
-        lowpass = mkBiquadCombo "LinkwitzRileyLowpass" 100 8;
-        sub_l_0 = mkBiquad "Peaking" { freq = 43.6; gain = -4.6; q = 2.781; };
-        sub_l_1 = mkBiquad "Peaking" { freq = 81.9; gain = -5.5; q = 1.498; };
-        sub_r_0 = mkBiquad "Peaking" { freq = 49.75; gain = -4.0; q = 3.22; };
-        sub_r_1 = mkBiquad "Peaking" { freq = 90.1; gain = -8.4; q = 1.0; };
-      };
-      pipeline = builtins.concatLists [
-        (mkPipelineMixer "main")
-        (mkPipelineFilter [0] [
-          "highpass"
-          #"left_unplugged_0"
-          #"left_unplugged_1"
-          #"left_unplugged_2"
-        ])
-        (mkPipelineFilter [1] [
-          "highpass"
-          #"right_unplugged_0"
-          #"right_unplugged_1"
-          #"right_unplugged_2"
-          #"right_unplugged_3"
-        ])
-        (mkPipelineFilter [2] [
-          "lowpass"
-          "sub_l_0"
-          "sub_l_1"
-        ])
-        (mkPipelineFilter [3] [
-          "lowpass"
-          "sub_r_0"
-          "sub_r_1"
-        ])
-      ];
-    };
   };
 
   hardware.gpio.enable = true;
