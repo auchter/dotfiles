45d1b7d7e6bc93d39396679cde72d7100bdaf331
Michael Auchter <a@phire.org>
2024-01-01 14:22:22 +0000

    balan: point towards new snapcast server

--- systems/balan/configuration.nix
+++ systems/balan/configuration.nix
@@ -17,7 +17,7 @@
 
   modules.snapclient = {
     enable = true;
-    host = "malphas";
+    host = "azazel.local.phire.org";
     sampleFormat = "44100:16:*";
     soundcard = "front:CARD=adapter,DEV=0";
   };
